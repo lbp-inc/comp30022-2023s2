1c01476683a964a05b8292f6a4e1c6f16905e979
LangzeL <langzel@student.unimelb.edu.au>
2023-10-03 18:58:01 +1100

    update UserMenu

--- frontend/src/HomePage/UserMenu.js
+++ frontend/src/HomePage/UserMenu.js
@@ -1,43 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './UserMenu.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
-function TopCorner({onClick}) {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-   
+function TopCorner() {  
     return (
         <div className="userMenuContainer">
-            <FontAwesomeIcon icon={faUser} 
-                             className="userIcon" 
-                             onMouseEnter={() => setIsDropdownOpen(true)} 
-                             onMouseLeave={() => setIsDropdownOpen(false)}
-            />
-
-            {isDropdownOpen && (
-                <div className="dropdownMenu" 
-                     onMouseEnter={() => setIsDropdownOpen(true)} 
-                     onMouseLeave={() => setIsDropdownOpen(false)}>
-                    
-                    <table className='menuItem'>
-                        <tr><td><Link to="/LogIn">Login</Link></td></tr>
-                        <tr><td>Register</td></tr>
-                    </table>
-
-                </div>
-            )}
-
+            <Link to="/LogIn">
+                <FontAwesomeIcon icon={faUser} 
+                 className="userIcon" 
+            /></Link>
         </div>
     );
 }
 
 export default TopCorner;
 
-// <div className="menuItem">
-// <Link to = "/LogIn" className="Link">
-// <button onClick={onClick} className="menuLoginButton">Login</button>
-// </Link>
-// </div>
-
-// <div className="menuItem">Register</div>
