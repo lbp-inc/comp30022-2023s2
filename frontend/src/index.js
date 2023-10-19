3d6eb59fffee6badf5af0cd29c676e7bc4fcbd12
m1nicrusher <19246983+m1nicrusher@users.noreply.github.com>
2023-10-19 20:49:11 +1100

    Add react-bootstrap library

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
