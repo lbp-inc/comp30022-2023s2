38cc3b1dd1f7fa5f4922501f8973525409ff6374
Stephen Zhang <19246983+m1nicrusher@users.noreply.github.com>
2023-10-20 21:51:13 +1100

    Add bookings schema

---
+++ backend/app/models/booking.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+// This schema describes a booking relationship between an activity and a member
+const bookingSchema = mongoose.Schema(
+    {
+        // Activity associated with this booking
+        activity_id: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+        },
+
+        // Member associated with this booking
+        member_id: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+        },
+
+        // Total amount of payment required (optional, default to 0 for free activities)
+        // This should not change once booking has made
+        // Outstanding payment should be calculated by adding transactions
+        // (manual credit adjustments can be made using virtual transactions with no member_id stated)
+        paymentRequired: Number,
+
+        // Records relevant transactions (to calculate outstanding payment)
+        // Note the transaction doesn't necessarily have to be made by this member
+        transaction_id: [mongoose.Types.ObjectId],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
