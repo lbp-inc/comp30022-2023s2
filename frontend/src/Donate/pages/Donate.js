f239f174db0d640a6fa346f428e9ab0b39350db7
Andylenguyen237 <lenguyenlinhdan02@gmail.com>
2023-10-24 09:49:23 +1100

    finished merging donate page

--- frontend/src/Donate/pages/Donate.js
+++ frontend/src/Donate/pages/Donate.js
@@ -8,19 +8,15 @@ function Donate() {
   return (
     <>
         <Layout>
-        <div className="hero-head-container">
-            <h1>Donation</h1>
+        <div className="donation-head-banner">
+            <h1>Donate</h1>
         </div>
         <div className="info-section">
         Donate and make a difference today. {'\n\n'}
         By donating to Longbeach Place you help us become closer to our goal of empowering people so they can realise their true potential. 
         Your donation enables us to responds to community needs by providing a range of structured educational programs, social activities, and special interest support groups. The programs and activities are developed through community consultation.  {'\n\n'}
         Donations can be anything such as stationary, art supplies, musical instruments the choice is yours. Every little bit helps. To donate please contact this address: {'\n'}
-        15 Chelsea Road
-        Chelsea 
-        Victoria 3196
-        Tel: 03 9776 1386
-        Email: reception@longbeachplace.org.au
+        15 Chelsea Road Chelsea Victoria 3196                Tel: 03 9776 1386                                                                      Email: reception@longbeachplace.org.au
         </div>
         <div className='hero-container'>
           <h1>Donate now</h1>
