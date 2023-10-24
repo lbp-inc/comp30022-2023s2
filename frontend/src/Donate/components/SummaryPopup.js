e8d21553c4797268f23a07167f9aa5e9a9f60d92
Alan <94225775+dtnm@users.noreply.github.com>
2023-10-25 10:43:27 +1100

    donateForm Popup display fixed

--- frontend/src/Donate/components/SummaryPopup.js
+++ frontend/src/Donate/components/SummaryPopup.js
@@ -7,23 +7,24 @@ import { Link, useNavigate } from "react-router-dom";
 
 function SummaryPopup({ formData, handleClose }) {
 
-   const navigator = useNavigate();
+  const navigator = useNavigate();
 
-  const handleSubmit =  (event) => {
+  const handleSubmit = (event) => {
     navigator("/DonationPageMessage");
   }
 
   return (
     <div className="summary-popup">
       <h1 className="heading">Thank you!</h1>
-      <ul className="content">
-        <li><strong>First Name:</strong> {formData.firstName}</li>
-        <li><strong>Last Name:</strong> {formData.lastName}</li>
-        <li><strong>Email:</strong> {formData.email}</li>
-        <li><strong>Phone Number:</strong> {formData.phoneNumber}</li>
-        <li><strong>Donation Amount:</strong> ${formData.donationAmount}</li>
-        <li><strong>Message:</strong> {formData.comments}</li>
-      </ul>
+      <table className="summary-details">
+        <tr><td><strong>First Name:</strong></td><td>{formData.firstName}</td></tr>
+        <tr><td><strong>Last Name:</strong></td><td>{formData.lastName}</td></tr>
+        <tr><td><strong>Email:</strong></td><td>{formData.email}</td></tr>
+        <tr><td><strong>Phone Number:</strong></td><td>{formData.phoneNumber}</td></tr>
+        <tr><td><strong>Donation Amount:</strong></td><td>${formData.donationAmount}</td></tr>
+        <tr><td><strong>Message:</strong></td><td>{formData.comments}</td></tr>
+      </table>
+      <br></br>
       <button onClick={handleClose}>Edit</button>
 
       <button onClick={handleSubmit}>Confirm</button>
