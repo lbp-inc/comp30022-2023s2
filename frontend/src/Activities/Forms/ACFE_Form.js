20d2d7f397ad6c76eaa8152a5e1e09ce1e0c9bbd
Hanny <zyhaierciyuan@gmail.com>
2023-10-28 00:13:03 +1100

    ACFE Form implementation half-way

--- frontend/src/Activities/Forms/ACFE_Form.js
+++ frontend/src/Activities/Forms/ACFE_Form.js
@@ -3,8 +3,12 @@ ACFE online form page [Using react-Bootstrap]
 From: Event detail page
 Jump to: Success page / Cancel page / Fail page
 */
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Accordion from 'react-bootstrap/Accordion';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './Forms.css';
 
 const ACFEForm = () => {
@@ -18,31 +22,94 @@ const ACFEForm = () => {
 
                 <Form>
                     {/* Form main body */}
-                    <Form.Group className="mb-3" controlId="ACFE.fname">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="name" placeholder="March" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="ACFE.lname">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="name" placeholder="Seven" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="ACFE.phoneNumber">
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type="phone" placeholder="0412345678" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="ACFE.email">
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com.au" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="IDcopy">
-                        <Form.Label>Upload a copy of your ID</Form.Label>
-                        <Form.Control type="file" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="">
-                        <Form.Label>More fields to be added!</Form.Label>
-                        <Form.Control type="" placeholder="Enter your ans!" rows={3} />
-                    </Form.Group>
-                    <Button variant="outline-primary">Register!</Button>{' '}
+                    <Accordion defaultActiveKey="0">
+
+                        {/* Personal Details Accordion including Emergency Contact */}
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Personal Details</Accordion.Header>
+                            <Accordion.Body>
+                            <Row>
+                                <Col>
+                                    <Form.Label>Prefix</Form.Label>
+                                    <Form.Select aria-label="Default select example">
+                                        <option>Select...</option>
+                                        <option value="Mr">Mr</option>
+                                        <option value="Mrs">Mrs</option>
+                                        <option value="Ms">Ms</option>
+                                        <option value="Dr">Dr</option>
+                                        <option value="">None</option>
+                                        <option value="Other">Other, please specify</option>
+                                    </Form.Select>
+                                </Col>
+                                <Col>
+                                    <Form.Label>Gender</Form.Label>
+                                    <Form.Select aria-label="Default select example">
+                                        <option>Select...</option>
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                        <option value="other">Other, please specify</option>
+                                    </Form.Select>
+                                </Col>
+                            </Row>
+
+                            <Form.Label></Form.Label>
+                            <FloatingLabel controlId="ACFE.fname" label="First Name">
+                                <Form.Control type="name" placeholder="March" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.sname" label="Surname">
+                                <Form.Control type="name" placeholder="Seven" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.dob" label="Date of Birth">
+                                <Form.Control type="date" placeholder="Seven" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.phoneNumber" label="Phone Number">
+                                <Form.Control type="phone" placeholder="0412345678" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.email" label="Email Address">
+                                <Form.Control type="email" placeholder="iLoveCos@starrail.me" />
+                            </FloatingLabel>
+                            <Form.Check
+                                type="switch"
+                                id="custom-switch"
+                                label="Subscribe for next Brochure? (Also available on our website!)"
+                            /><br></br>
+                            <FloatingLabel controlId="ACFE.address" label="Address">
+                                <Form.Control type="address" placeholder="15 Chelsea Rd" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.suburb" label="Suburb">
+                                <Form.Control type="suburb" placeholder="Chelsea" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.postcode" label="Post Code">
+                                <Form.Control type="postcode" placeholder="3196" />
+                            </FloatingLabel><br></br>
+                            <h3>Emergency Contact</h3>
+                            <FloatingLabel controlId="ACFE.ECname" label="Emergency Contact Name">
+                                <Form.Control type="name" placeholder="Pom-Pom" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.ECrelationship" label="Relationship to you">
+                                <Form.Control type="relationship" placeholder="" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.ECphoneNumber" label="Phone Number">
+                                <Form.Control type="phone" placeholder="0412345678" />
+                            </FloatingLabel><br></br>
+                            <h3>Concession Cards</h3>
+                            <FloatingLabel controlId="ACFE.crn" label="Centrelink CRN (Costomer Refrence Number)">
+                                <Form.Control type="number" placeholder="123 456 789 0" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.cardType" label="Card Type (e.g. PPS/NS/DSP)">
+                                <Form.Control type="cardType" placeholder="NS" />
+                            </FloatingLabel><br></br>
+                            <FloatingLabel controlId="ACFE.expiry" label="Expiry Date">
+                                <Form.Control type="date" />
+                            </FloatingLabel>
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        {/* Citizen Check Accordion */}
+                        <Accordion.Item eventKey="1">
+                            {/* TODO: */}
+                        </Accordion.Item>
+                    </Accordion>
                 </Form>
             </div>
         </>
